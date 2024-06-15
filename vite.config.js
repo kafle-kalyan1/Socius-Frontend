0c639a5608c15ee422b58cc2e81b13182d1b6f90
kalyan.kafle <kalyan.kafle@aegishms.com>
2024-06-15 14:18:28 +0545

    refactor: Update API endpoint rewrite logic to remove leading '/api'

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
         target: "https://socius.onrender.com/",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace("/api", ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
